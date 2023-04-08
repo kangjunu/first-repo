434b76e7cd7802ad2d6e62339f7b1f01a3ac2721
baldy <twinklee12@naver.com>
2023-04-08 16:07:13 +0900

    first commit

---
+++ dictionary1.py
@@ -0,0 +1,7 @@
+temp={
+}
+temp['a']='이지현'
+print(temp)
+temp['name']='강준우'
+print(temp)
+print(temp['a'])
---
+++ dictionary2.py
@@ -0,0 +1,9 @@
+name={}
+
+name={1:'강준우',2:'강혜인',3:'이지현'}
+
+a = name.keys()
+print(a)
+print(4 in name)
+s1=set("강준강우")
+print(s1)
